13b63290a57b17c92e832f0094064de82fc2f3cf
Max <simulacronzen@proton.me>
2026-04-14 17:02:52 +0200

    Ajout json+ modif preferences+informations

---
+++ script.js
@@ -0,0 +1,11 @@
+//Enregistrer élements
+
+let mode = document.getElementById('themeSelector').value
+localStorage.setItem('mode', mode)
+
+let affichage = document.getElementById('list').value
+console.log(affichage)
+let affichage2 = document.getElementById('list').value
+console.log(affichage2)
+
+
