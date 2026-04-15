4067bdedca82b450117c1a1290c6e153b3fcf8de
Max <simulacronzen@proton.me>
2026-04-15 12:10:16 +0200

    Préférences JS+JSON+création carte.html

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
-//Enregistrer élements
+//LocalStaorage clear après chargement de la page Preferences
 
-let mode = document.getElementById('themeSelector').value
-localStorage.setItem('mode', mode)
+window.addEventListener('load', function() {
+    if (window.location.pathname.endsWith('preferences.html')) {
+        localStorage.clear();
+        console.log('localStorage vidé');
+    }
+});
 
-let affichage = document.getElementById('list').value
-console.log(affichage)
-let affichage2 = document.getElementById('list').value
-console.log(affichage2)
+//Enregistrer profile
+
+function saveProfile(){
+    let mode = document.getElementById('themeSelector').value
+    let affichage = document.querySelector('input[name="displayChoice"]:checked').value
+    console.log(mode,affichage)
+    localStorage.setItem('mode', mode)
+    localStorage.setItem('affichage', affichage)
+    alert("Préférences enregistrées !")
+}
+
+
+//Tableau JSON
+
+fetch('promo.json').then(reponse => reponse.json()).then(apprenants => afficheData(apprenants))
+
+function afficheData(apprenants){
+    console.log(apprenants)
+}
 
 
